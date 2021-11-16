03f08940ad4216b6d0c16d259063b19def22520a
Ronya <klmirg6@gmail.com>
2021-11-16 02:38:28 -0600

    Started adding JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// Create click events for the search button
+// Add the API's
+var targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 5);
+console.log(targetDate);
+
+var getWeather = function() {
+
+  var apiUrl = 
+  console.log("function was called")
+}
+
+
+
+
+
+
+getWeather();
