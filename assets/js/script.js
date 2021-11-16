42314c393f0f0c1f54c360348f814535205099c7
Ronya <klmirg6@gmail.com>
2021-11-16 04:03:39 -0600

    added the api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 
 // Create click events for the search button
 // Add the API's
+var myKey = "304018960801880d53656057e380d93a";
 var targetDate = new Date();
 targetDate.setDate(targetDate.getDate() + 5);
 console.log(targetDate);
 
 var getWeather = function() {
 
-  var apiUrl = 
-  console.log("function was called")
+  // var apiUrl =
+ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=304018960801880d53656057e380d93a").then(function(response) {
+   response.json().then(function(data) {
+     console.log(data);
+   })
+ });
+  
 }
 
 
