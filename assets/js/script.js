97b586314d7a3c91ad95a9a760674501ba452160
Ronya <klmirg6@gmail.com>
2021-11-16 23:14:50 -0600

    Got information to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,7 @@ var cityInputEl = document.querySelector(".form-input")
 var weatherContainerEl = document.querySelector("#weather-container");
 var forecastContainerEl = document.querySelector("#forecast-container");
 // var forecastSearchEl = document.querySelector("#forecast");
+ 
 
 var targetDate = new Date();
 targetDate.setDate(targetDate.getDate() + 5);
@@ -43,15 +44,15 @@ var searchSubmitHandler = function(event) {
 
 
 
-var getWeather = function(city) {
+var getWeather = function(citySearch) {
 
   // var citySearch = cityInputEl.value.trim();
 
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=304018960801880d53656057e380d93a";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + myKey + "&units=imperial";
  fetch(apiUrl).then(function(response) {
    response.json().then(function(data) {
-     console.log(data);
-     displayWeather(data, city);
+    //  console.log(data);
+     displayWeather(data, citySearch);
    })
  });
 }
@@ -61,31 +62,38 @@ var getForecast = function(city) {
   var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=304018960801880d53656057e380d93a";
   fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
-      console.log(data);
+      console.log(data)
       displayForecast(data, city);
     })
   })
 }
-// When I search for a city, then I am presented with...
-// current and future conditions for that city and the city is added to the search history
-// When I view current weather conditions for that city
-// I am presented with the city name, the date, an icon representation of weather conditions,
-// the temperature, the humidity, the wind speed, and the UV Index
-// When I view the UV Index,
-// I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// When I view future weather conditions for that city
-// I am presented with a 5-day forecast that displays the date, an icon representations of 
-// weather conditions, the temperature, the wind speed, and the humidity
-// When I click on a city in the search history
-// I am presented with current and future conditions for that city
 
-var displayWeather = function() {
+// Figure out how to display the temperature, humidity, wind speed, and the UV index
+var displayWeather = function(weather, searchTerm) {
+
+  // clearing what is in the container
+  weatherContainerEl.textContent = "";
+  forecastContainerEl.textContent = searchTerm;
+
 
   var tempEl = document.createElement("div");
-  tempEl.classList = "justify-space-between";
+  tempEl.textContent = "Temperature: " + weather.main.temp;
+  weatherContainerEl.appendChild(tempEl);
+  
 
-  var 
-}
+  var humidityEl = document.createElement("div");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity;
+  weatherContainerEl.appendChild(humidityEl);
+
+  var windSpeedEl = document.createElement("div");
+  windSpeedEl.textContent = 
+
+  // var uvIndexEl = document.createElement("div");
+  // div.textContent = 
+
+
+
+};
 
 var displayForecast = function() {
   
@@ -103,6 +111,5 @@ var displayPreviousSearch = function(cities, searchForm) {
 }
 
 // getWeather();
-displayWeather ();
 
 citySearchFormEl.addEventListener("submit", searchSubmitHandler);
